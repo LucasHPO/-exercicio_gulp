50b2b55fc801070d36f4092fc7d736626890cd22
Lucas Hiago <lucashiago620@hotmail.com>
2023-03-31 15:53:44 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin')
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+function comprimeIMG(){
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'));
+}
+
+function comprimeJS(){
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'));
+}
+
+exports.default = function(){
+    gulp.watch('./source/styles/*.scss'), {igonereInitial: false}, gulp.series(compilaSass());
+    gulp.watch('./source/images/*'), {igonereInitial: false}, gulp.series(comprimeIMG());
+    gulp.watch('./source/scripts/*.js'), {igonereInitial: false}, gulp.series(comprimeJS());
+}
